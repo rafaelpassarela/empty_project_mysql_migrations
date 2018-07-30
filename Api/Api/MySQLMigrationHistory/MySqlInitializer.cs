623e0b2e0c7a09232c2680a9843f9fc3801585f1
Rafael Passarela <rafaelpassarela@gmail.com>
2018-07-30 14:40:38 -0300

    Auto Trigger Migration

--- Api/Api/MySQLMigrationHistory/MySqlInitializer.cs
+++ Api/Api/MySQLMigrationHistory/MySqlInitializer.cs
@@ -23,12 +23,15 @@ namespace Api.MySQLMigrationHistory
                 // query to check if MigrationHistory table is present in the database 
                 var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(sql);
 
-                // if MigrationHistory table is not there (which is the case first time we run) - create it
-                if (migrationHistoryTableExists.FirstOrDefault() == 0)
-                {
-                    context.Database.Delete();
-                    context.Database.Create();
-                }
+                // the ExecuteStoreQuery above, will trigger the mogration process of our database
+
+
+                //// if MigrationHistory table is not there (which is the case first time we run) - create it
+                //if (migrationHistoryTableExists.FirstOrDefault() == 0)
+                //{
+                //    context.Database.Delete();
+                //    context.Database.Create();
+                //}
             }
         }
     }
