78b2ad6d1dc0c942a9ee538b376a36802a1239b8
Rafael Passarela <rafaelpassarela@gmail.com>
2018-12-17 02:19:41 -0200

    Fix DataBase Creation
    If migration error happens, "Unable to update database to match the current model because there are pending changes and automatic migration is disabled", delete the migration folder (keep the config file) and run the command on Package Manager Console "Add-Migration Initial"

--- backend-api/Api/Migrations/201812170412362_Initial.Designer.cs
+++ backend-api/Api/Migrations/201812170412362_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Api.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class MyFirstMigration : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(MyFirstMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201807171729286_MyFirstMigration"; }
+            get { return "201812170412362_Initial"; }
         }
         
         string IMigrationMetadata.Source
