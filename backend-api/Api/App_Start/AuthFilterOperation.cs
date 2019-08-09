59a16bfa5a100fcbd91e41b4e74fb56e89805307
Rafael Passarela <rafaelpassarela@gmail.com>
2019-08-09 18:46:38 -0300

    Add Swagger config and enabled OperationFilter and DocumentFilter

---
+++ backend-api/Api/App_Start/AuthFilterOperation.cs
@@ -0,0 +1,26 @@
+﻿using Swashbuckle.Swagger;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+
+namespace Api.App_Start
+{
+    public class AuthFilterOperation : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "acess token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
